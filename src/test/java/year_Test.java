f77648c755544e7fcb1d151fff104a036f4d8ed6
Odai Mohammed Alkhalaf Alissa <112479069+odaia2@users.noreply.github.com>
2022-10-27 15:17:15 +0200

    Update year_Test.java

--- src/test/java/year_Test.java
+++ src/test/java/year_Test.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class year_Test {
 
     @Test
-    public void when_divided_by_4_not_100() {assertEquals( false, year.isLoopYear(1916));}
+    public void when_divided_by_4_not_100() {assertEquals( true, year.isLoopYear(1916));}
     @Test
     public void not_divides_by_4() {assertEquals(false, year.isLoopYear(1800));
     }
